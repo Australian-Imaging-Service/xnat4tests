86aaa6e762fdd8a1b037e0dee5510f75fff1615d
Tom Close <tom.g.close@gmail.com>
2022-01-17 16:20:15 +1100

    added versioneer to repository

--- xnat4tests/__init__.py
+++ xnat4tests/__init__.py
@@ -1,3 +1,6 @@
 from .launch import (
     launch_xnat, launch_docker_registry, stop_xnat, stop_docker_registry,
     connect)
+
+from . import _version
+__version__ = _version.get_versions()['version']
