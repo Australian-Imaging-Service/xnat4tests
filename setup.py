f37022792f45f080b7bdbcc0372b2824742f9698
Tom Close <tom.g.close@gmail.com>
2022-11-12 09:01:03 +1100

    added pytest-cov to tests require

--- setup.py
+++ setup.py
@@ -2,29 +2,33 @@ import versioneer
 from setuptools import setup, find_packages
 
 
-PYTHON_VERSIONS = ['3.7', '3.8', '3.9', '3.10']
+PYTHON_VERSIONS = ["3.7", "3.8", "3.9", "3.10"]
 
 
 setup(
-    name='xnat4tests',
+    name="xnat4tests",
     version=versioneer.get_version(),
-    author='Thomas G. Close',
-    author_email='tom.g.close@gmail.com',
+    author="Thomas G. Close",
+    author_email="tom.g.close@gmail.com",
     packages=find_packages(),
-    url='https://github.com/australian-imaging-service/xnat4tests',
-    license='CC0',
-    description=('Creates basic XNAT instance for API tests'),
-    long_description=open('README.rst').read(),
+    url="https://github.com/australian-imaging-service/xnat4tests",
+    license="CC0",
+    description=("Creates basic XNAT instance for API tests"),
+    long_description=open("README.rst").read(),
     install_requires=[
-        'docker>=5.0.2',
-        'xnat>=0.3.17',
-        'click>=8.1.3',
-        'requests>=2.10.0',
-        'medimages4tests>=0.3',
-        'PyYAML>=6.0'],
+        "docker>=5.0.2",
+        "xnat>=0.3.17",
+        "click>=8.1.3",
+        "requests>=2.10.0",
+        "medimages4tests>=0.3",
+        "PyYAML>=6.0",
+    ],
     extras_require={
-        'test': [
-            'pytest>=5.4.3']},
+        "test": [
+            "pytest>=5.4.3",
+            "pytest-cov>=2.12.1",
+        ]
+    },
     entry_points={
         "console_scripts": [
             "xnat4tests=xnat4tests.cli:cli",
@@ -34,13 +38,16 @@ setup(
     include_package_data=True,
     cmdclass=versioneer.get_cmdclass(),
     classifiers=(
-        ["Development Status :: 4 - Beta",
-         "Intended Audience :: Healthcare Industry",
-         "Intended Audience :: Science/Research",
-         "License :: OSI Approved :: Apache Software License",
-         "Natural Language :: English",
-         "Topic :: Scientific/Engineering :: Bio-Informatics",
-         "Topic :: Scientific/Engineering :: Medical Science Apps."]
-        + ["Programming Language :: Python :: " + str(v)
-           for v in PYTHON_VERSIONS]),
-    keywords='repository analysis neuroimaging workflows pipelines')
+        [
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Healthcare Industry",
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: Apache Software License",
+            "Natural Language :: English",
+            "Topic :: Scientific/Engineering :: Bio-Informatics",
+            "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        ]
+        + ["Programming Language :: Python :: " + str(v) for v in PYTHON_VERSIONS]
+    ),
+    keywords="repository analysis neuroimaging workflows pipelines",
+)
