71aa06a5eac02efb05523a84b7221c75e1fb0687
Tom Close <tom.g.close@gmail.com>
2022-11-12 08:51:08 +1100

    fixed up add_data unittest

--- tests/test_data.py
+++ tests/test_data.py
@@ -7,6 +7,14 @@ def test_add_data(config, launched_xnat):
     add_data("dummydicom", config_name=config)
 
     with connect(config) as login:
-        xsess = login.projects["dummydicomproject"].subjects['dummydicomsubject'].experiments['dummydicomsession']
+        xsess = (
+            login.projects["dummydicomproject"]
+            .subjects["dummydicomsubject"]
+            .experiments["dummydicomsession"]
+        )
 
-    assert sorted(s.type for s in xsess.scans()) == ["t1w", "dwi", "fmap"]
+        assert sorted(s.type for s in xsess.scans.values()) == [
+            "R-L MRtrix 60 directions interleaved B0 ep2d_diff_p2",
+            "gre_field_mapping 3mm",
+            "t1_mprage_sag_p2_iso_1",
+        ]
