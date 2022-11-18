10ffb8e1e922b00f94525264539a1308a1103773
Tom Close <tom.g.close@gmail.com>
2022-11-19 09:22:35 +1100

    added xnat_user and password to base config

--- xnat4tests/config.py
+++ xnat4tests/config.py
@@ -49,6 +49,8 @@ class Config:
         factory=dict, converter=lambda d: BuildArgs(**d)
     )
     loaded_from: Path = None
+    xnat_user: str = "admin"
+    xnat_password: str = "admin"
 
     @xnat_port.validator
     def xnat_port_validator(self, _, xnat_port):
