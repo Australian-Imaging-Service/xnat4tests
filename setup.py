2362efa3314459c5e579a1ec650dd508d9242824
Tom Close <tom.g.close@gmail.com>
2022-11-30 17:27:24 +1100

    relaxed click dependency to 7.1.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=[
         "docker>=5.0.2",
         "xnat>=0.3.17",
-        "click>=8.1.3",
+        "click>=7.1.2",
         "requests>=2.10.0",
         "medimages4tests>=0.3",
         "PyYAML>=6.0",
